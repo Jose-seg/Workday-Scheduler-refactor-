4e7eb3e13601d3024900c60bcf704a2d552fb1b9
Jose Segura <jossegura2023@u.northwestern.edu>
2023-02-26 14:43:48 -0600

    added finishing touches and readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Workday-Scheduler-refactor-
+## Description 
+This is a refactor of a week day scheduler that was supposed to be dynamically coded using Javascript. When the user views the site now they are able to see the current day and I also included the current time as well, and each business hour for that day is displayed on the bottom of the page. Each hour must be color coordinated according whether its past, present, future, and it had to be that each time block changes colors in regards to the current time. Lastly the goal was to save user input in local storage and display the user input on the textarea box so it persist on the page even if the user were to refresh the page.
+
+## Usage 
+The usage for this application is to provide a user with a dynamically driven week day scheduler, that uses a dynamic build in order to provide a scheduler that runs on current time and updates according to the current hour. This application provides users with a way to plan their day with this application. This application uses dayjs libraries and jquery in order to make it functional, and it served a great way of learning to use dayjs libraries and jquery.
+
+## Installation
+
+bootstrap: https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css
+
+fontawesome: https://use.fontawesome.com/releases/v5.8.1/css/all.css
+
+google fonts: https://fonts.googleapis.com/css?family=Open+Sans&display=swap
+
+jquery: https://code.jquery.com/jquery-3.4.1.min.js
+
+dayjs library: https://cdn.jsdelivr.net/npm/dayjs@1.11.3/dayjs.min.js
+
+## ScreenShots
+![image](https://user-images.githubusercontent.com/122575280/221435816-9eaf0ef3-ebc6-49fe-a4e1-cc97fd90e085.png)
+![image](https://user-images.githubusercontent.com/122575280/221435829-2b87d17d-b89e-4063-be9c-089b8db85088.png)
+![image](https://user-images.githubusercontent.com/122575280/221435903-93dae621-78d8-48fa-b623-170f12a2b9da.png)
+![image](https://user-images.githubusercontent.com/122575280/221435946-c280a5e3-a475-4eb4-80ad-6d162a3340b8.png)
+
+## Deployment
+
+
+## Github Repo
+https://github.com/Jose-seg/Workday-Scheduler-refactor-
+
+
+
+
+
--- develop/script.js
+++ develop/script.js
@@ -1,7 +1,6 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-// var saveBtn = $('#saveBtn');
 
 $(document).ready(function () {
   // Adds event listener for all save buttons
@@ -15,18 +14,20 @@ $(document).ready(function () {
 // Get current date and display in header
 var currentDay = dayjs().format('dddd, MMMM D');
 $('#currentDay').text(currentDay);
-var hourNow = dayjs().format('hh:mm:ss A');
+var hourNow = dayjs().format('HH:mm:ss A');
 $('#hourNow').text(hourNow);
 
 // Current hour in 24 hour time
 var currentHour = dayjs().hour();
+console.log(currentHour);
 
 // Iterates for each time block and applies past, present,future
 $('.time-block').each(function() {
-  // Get the hour ID of current time block
-  // var hourId = $(this).attr('id');
+  // Get the hour ID of current time block also for the textare value to remain on the page
+  var hourId = $(this).attr('id');
   // Take hour number from ID ex. hour-9 => 9
-  var blockHour = parseInt($(this).attr('id').split('-')[1]);
+  // var blockHour = parseInt($(this).attr('id').split('-')[1]);
+  var blockHour = parseInt(hourId.split('-')[1]);
   // Apply past, present, future classes by comparison to current hour
   if(blockHour < currentHour) {
     $(this).addClass('past');
@@ -44,24 +45,4 @@ $(this).children('.description').val(userInput);
 
 });
 
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
